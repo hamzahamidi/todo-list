fe5cd1669d377d7b0e12f53c715411fcd467935c
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-01-24 01:11:30 +0100

    Use Async
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Item, TodoList } from '../../models/todo-list';
 import { TodoListProvider } from '../../providers/todo-list';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  todoLists: TodoList[];
+  todoLists$: Observable<TodoList[]>;
   items: Item[] = [];
   test;
   constructor(public navCtrl: NavController, public _TodoListProvider: TodoListProvider) {
@@ -16,6 +17,6 @@ export class HomePage {
   }
 
   ngOnInit() {
-    this._TodoListProvider.getTodoList().subscribe(todoList => this.todoLists = todoList);
+    this.todoLists$ = this._TodoListProvider.getTodoList();
   }
 }
