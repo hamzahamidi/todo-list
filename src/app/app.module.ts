08d32d2e433b74f38299a40d2e2d0bcf7347f463
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-01-27 19:32:40 +0100

    Delete unused pages
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,10 +14,7 @@ import { TodoListProvider } from '../providers/todo-list';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -31,10 +25,7 @@ import { TodoListProvider } from '../providers/todo-list';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   providers: [
     StatusBar,
