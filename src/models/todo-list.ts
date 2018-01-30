087b3b54f9d10b4ef1ae759ba406d5105a0f68a2
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-01-30 02:55:23 +0100

    Change data models
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- src/models/todo-list.ts
+++ src/models/todo-list.ts
@@ -1,13 +1,18 @@
 export class TodoList {
     id?: string;
-    name:string;
-    items: Item[];
-} 
+    name: string;
+    items: Set<Item>
+}
 
 export class Item {
     id?: string;
     name: string;
     state: boolean;
     description: string;
+    constructor(name: string, state: boolean, description: string) {
+        this.name = name;
+        this.state = state;
+        this.description = description;
+    }
 }
 
