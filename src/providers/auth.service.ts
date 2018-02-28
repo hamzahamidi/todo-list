08ef6f1b69d0b0afdea8345b4af6baa3806ca5f6
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-02-28 16:06:59 +0100

    Add sign in /out
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- src/providers/auth.service.ts
+++ src/providers/auth.service.ts
@@ -1,52 +1,85 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
-import { User } from '../models/user';
+import { User } from '../models';
 import { GooglePlus } from '@ionic-native/google-plus';
+import { Platform } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class AuthProvider {
   user: Observable<User>;
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private googlePlus: GooglePlus) { }
-  /**
-   * JS method 
-   * No longer used in this application
-   */
-  googleLogin(): Promise<void> {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    return this.oAuthLogin(provider);
-  }
-  private oAuthLogin(provider): Promise<void> {
-    return this.afAuth.auth.signInWithPopup(provider)
-      .then((credential) => {
-        this.updateUserData(credential.user);
-      })
-  }
-  private updateUserData(user): Promise<void> {
-    // Sets user data to firestore on login
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-    const data: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL
+  constructor(private plt: Platform, private afAuth: AngularFireAuth, private db: AngularFireDatabase, private googlePlus: GooglePlus) { }
+
+  signInGoogle(): Promise<User> {
+    //on a device running Android or on a device running iOS.
+    if (this.plt.is('ios') || this.plt.is('android')) {
+      return this.nativeloginUser();
     }
-    return userRef.set(data);
-  }
-  signOut() {
-    return this.afAuth.auth.signOut();
+    // on a desktop device.
+    else if (this.plt.is('core')) {
+      return this.browserGoogleLogin();
+    }
   }
 
   /**
+   * JS method 
+   */
+  browserGoogleLogin(): Promise<User> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider);
+  }
+  oAuthLogin(provider): Promise<User> {
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then(credential => this.updateUserData(credential))
+  }
+  /**
    * native method GooglePlus
    */
-  loginUser(): Promise<void> {
+  nativeloginUser(): Promise<User> {
     return this.googlePlus.login({
-      'webClientId': '588101161325-f3p5ulsoe22ok3gbblq8itej4msbfvht.apps.googleusercontent.com',
-      'offline': true
-    });
+      'webClientId': '588101161325-f3p5ulsoe22ok3gbblq8itej4msbfvht.apps.googleusercontent.com'
+    })
+      .then(provider => this.oAuthNativeLogin(provider));
+  }
+
+  oAuthNativeLogin(provider): Promise<User> {
+    const googleCredential = firebase.auth.GoogleAuthProvider.credential(provider.idToken);
+    return this.afAuth.auth.signInAndRetrieveDataWithCredential(googleCredential)
+      .then(credential => this.updateUserData(credential));
+  }
+
+  updateUserData(credential): Promise<User> {
+    //console.log('credential:', credential);
+    let user: User = {
+      uid: credential.user.uid,
+      email: credential.user.email,
+      displayName: credential.user.displayName,
+      photoURL: credential.user.photoURL
+    }
+    //console.log('user:', user);
+    // Check if new user
+    if (credential.additionalUserInfo.isNewUser) {
+      // Sets user data to firebase on login
+      this.db.list(`/users/`).set(user.uid, user);
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    return new Promise(resolve => resolve(user));
+  }
+
+  checkConnection(): boolean {
+    return !!localStorage.getItem("user");
+  }
+
+  getUserData() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return this.db.object(`/users/${user.uid}`).valueChanges();
+  }
+
+  signOut() {
+    return this.afAuth.auth.signOut()
+    .then(res => localStorage.clear());
   }
 }
