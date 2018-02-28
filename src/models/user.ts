bf6a05148811dc417b57fcf574ca006a6ad6d50a
Rajaa-BARHTAOUI <rajaa.barhtaoui@gmail.com>
2018-02-28 13:26:12 +0000

    update models

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,10 @@
+import { TodoList } from "./todo-list";
+
 export interface User {
-    uid: string;
-    email: string;
-    photoURL?: string;
-    displayName?: string;
-    favoriteColor?: string;
-  }
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName?: string;
+  favoriteColor?: string;
+  todoList?: TodoList[];
+}
